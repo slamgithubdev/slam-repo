427c4d85084645724ca8c2088c727b65458a4a70
Danesh Paul <Danesh.Paul@billingfinance.co.uk>
2025-10-20 18:09:26 +0100

    parsing new data

---
+++ tuum_data_import/dataclass_person.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import List
+
+
+@dataclass
+class ValidityRange:
+    startTime: str
+    endTime: str
+
+
+@dataclass
+class Address:
+    addressTypeCode: str
+    street1: str
+    street2: str
+    cityCounty: str
+    stateRegion: str
+    zip: str
+    countryCode: str
+    moveInDate: str
+    validityRange: ValidityRange
+
+
+@dataclass
+class IdentificationNumber:
+    idNumber: str
+    idCountryCode: str
+    primary: bool
+    validityRange: ValidityRange
+
+
+@dataclass
+class Source:
+    sourceName: str
+    sourceRef: str
+
+
+@dataclass
+class Employment:
+    personId: str
+    jobTitle: str
+    employer: str
+    yearsAt: int
+    monthsAt: int
+
+
+@dataclass
+class Person:
+    externalPersonId: str
+    source: Source
+    personTypeCode: str
+    givenName: str
+    middleName: str
+    surname: str
+    name: str
+    birthDate: str
+    email: str
+    phoneNumberCountryCode: str
+    phoneNumber: str
+    addresses: List[Address] = field(default_factory=list)
+    identificationNumbers: List[IdentificationNumber] = field(default_factory=list)
+    # employmentHistory: List[Employment] = field(default_factory=list)
